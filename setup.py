95fc6d97ff35612ea1fe91314eefc56bb6fb1ec9
Tomotaka Ito <tomotaka.ito@gmail.com>
2014-11-18 21:49:07 +0900

    bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bottle-api',
-    version='0.0.1',
+    version='0.0.2',
     description='Semantic Web Challenge 2014',
     author='Tomotaka Ito',
     author_email='tomotaka.ito@gmail.com',
