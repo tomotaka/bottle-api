73250098201e1b6ae9e911c79b68669393314069
Tomotaka Ito <tomotaka.ito@gmail.com>
2014-12-04 16:53:49 +0900

    bump version => 0.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bottle-api',
-    version='0.0.2',
+    version='0.0.3',
     description='useful decorator for building WebAPI on Bottle web framework development',
     author='Tomotaka Ito',
     author_email='tomotaka.ito@gmail.com',
